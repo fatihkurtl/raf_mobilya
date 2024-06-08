e90262152faa43743adc69450be46ee05a9289fa
fatihkurtl <kurtf061@gmail.com>
2024-06-08 16:58:58 +0300

    home page divided into parts

---
+++ src/components/home/AdsSpace.tsx
@@ -0,0 +1,15 @@
+const AdsSpaceComp = () => {
+  return (
+    <div className="container pb-16">
+      <a href="#">
+        <img
+          src="../../src/assets/images/offer.jpg"
+          alt="ads"
+          className="w-full"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default AdsSpaceComp;
